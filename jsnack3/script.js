061ebd00035186d01b49bfa1ccce3a4315a616f4
StefanoMast <ste.mastrantonio@gmail.com>
2022-04-20 15:08:26 +0200

    add jsnack3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,24 +1,11 @@
 //Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente.
 
 
-//chiedere all'utente quanti numeri vuole stampare
-let userNumberPrint = [];
-const userNumber = parseInt(prompt("dimmi un numero"));
-console.log(userNumber);
+//chiedere all'utente quanti numeri vuole calcolare
+const N = parseInt(prompt("dimmi un numero"));
 
 //faccio il ciclo for per quanti numeri l'utente vuole stampare
-for (let index = 1; index <= userNumberPrint.length; index++) {
-    
-    
-    
-    let x = userNumber;
-    console.log(x);
-    
-    let z = userNumber ** 3;
-    console.log(z);
-    
+for (let index = 1; index <= N; index++) {
+    let cubo = index ** 3;
+    console.log(cubo);
 }
-
-//chiedere un numero all'utente
-
-
